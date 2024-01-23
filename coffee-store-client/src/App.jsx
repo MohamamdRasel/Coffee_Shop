e4d09553e41a5710c178193904d499198508dfff
Mohammad <mohammadrasel201@gmail.com>
2024-01-24 05:53:45 +0600

    Added Update caffee

--- coffee-store-client/src/App.jsx
+++ coffee-store-client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className=' m-20'>
       
-      <h1 className=' text-6xl text-center text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1>
+      <h1 className=' text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1>
       <div className=' grid md:grid-cols-2 gap-4'>
         {
           coffees.map(coffee => <CoffeeCard
