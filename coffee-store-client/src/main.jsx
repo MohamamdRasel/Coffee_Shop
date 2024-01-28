a4486e09f8dfaaf45f192f16143c72f0cce33da4
Mohammad <mohammadrasel201@gmail.com>
2024-01-28 10:43:28 +0600

    added users

--- coffee-store-client/src/main.jsx
+++ coffee-store-client/src/main.jsx
@@ -11,6 +11,7 @@ import UpdateCoffee from './components/UpdateCoffee.jsx';
 import SignUp from './components/SignUp.jsx';
 import SignIn from './components/SignIn.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
+import Users from './components/Users.jsx';
 // import AuthProvider from './assets/providers/AuthProvider.jsx';
 
 const router = createBrowserRouter([
@@ -35,6 +36,11 @@ const router = createBrowserRouter([
   {
     path: '/signin',
     element: <SignIn></SignIn>
+  },
+  {
+    path:'/users',
+    element: <Users></Users>,
+    loader: () => fetch('http://localhost:5000/user')
   }
 ]);
 
